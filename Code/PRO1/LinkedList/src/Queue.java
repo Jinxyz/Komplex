29be4f53b530eb00264c9b413c9e4d728e350f5c
Jinxyz <xxyaymanyayxx@hotmail.com>
2020-02-19 13:42:21 +0100

    Arbetat på linked list, addLast är klar

---
+++ Code/PRO1/LinkedList/src/Queue.java
@@ -0,0 +1,23 @@
+public class Queue {
+    // Need data structure....
+
+    public Queue() {
+
+    }
+
+    public void enqueue(String text) {
+
+    }
+
+    public String dequeue() {
+        return "";
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return -1;
+    }
+}
